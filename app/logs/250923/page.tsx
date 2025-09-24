3d3165a41065341dcc61f7624251aca03fe17c17
hanbin <rj2303@naver.com>
2025-09-24 22:31:58 +0900

    log

---
+++ app/logs/250923/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main>
+      <p>부자가 가난까지 훔쳐간다.</p>
+      <p>원하지 않아도 가질 수 있다.</p>
+      <p>있는 사람은 더 가지게 되고, 없는 사람은 가진 것마저 잃는다.</p>
+    </main>
+  );
+}
