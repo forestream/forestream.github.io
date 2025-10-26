9e02495f2dc829dff506350f05ebf3e3457146bd
hanbin <rj2303@naver.com>
2025-10-26 10:30:44 +0900

    fix configs

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createMDX from "@next/mdx";
 const nextConfig: NextConfig = {
   output: "export",
   pageExtensions: ["ts", "tsx", "md", "mdx", "jsx", "js"],
+  turbopack: {
+    resolveExtensions: [".ts", ".tsx", ".md", ".mdx", ".jsx", ".js"],
+  },
 };
 
 const withMDX = createMDX({
