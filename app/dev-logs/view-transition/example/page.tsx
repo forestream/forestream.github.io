217dd1e078fc67ae1cddc6daee0dc76d2f947efd
hanbin <rj2303@naver.com>
2025-11-09 14:34:39 +0900

    add log

--- app/dev-logs/view-transition/example/page.tsx
+++ app/dev-logs/view-transition/example/page.tsx
@@ -31,10 +31,54 @@ export default async function ExamplePage() {
 
   return (
     <>
+      <style>
+        {`
+@view-transition {
+navigation: auto;
+}
+
+@keyframes move-out {
+from {
+transform: translateY(0%);
+}
+
+to {
+transform: translateY(-100%);
+}
+}
+
+@keyframes move-in {
+from {
+transform: translateY(100%);
+}
+
+to {
+transform: translateY(0%);
+}
+}
+
+/* Apply the custom animation to the old and new page states */
+::view-transition-old(root) {
+animation: 0.4s ease-in both move-out;
+}
+
+::view-transition-new(root) {
+animation: 0.4s ease-in both move-in;
+}
+      `}
+      </style>
       <div
         className="[&_button]:cursor-pointer [&_button]:border [&_button]:px-2"
         dangerouslySetInnerHTML={{ __html: page.toString() }}
       />
+      <div className="mt-4">
+        <a
+          href="/dev-logs/view-transition/example/child"
+          className="underline duration-100 hover:text-slate-400"
+        >
+          MPA View Transition - Go to Child Page
+        </a>
+      </div>
       <span className="mt-10 inline-block text-xs text-slate-500">
         View Transition 학습용 페이지. 스타일 외에 기능 없음
       </span>
