6a740b0cbc2c4dd33ed8c631b74492bcc4778e40
hanbin <rj2303@naver.com>
2025-11-09 16:13:14 +0900

    update comp structure

--- app/_components/sections.tsx
+++ app/_components/sections.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode, useEffect, useState } from "react";
-
+import { usePathname } from "next/navigation";
 export function Sections({
   render,
 }: {
@@ -13,11 +13,12 @@ export function Sections({
     key: string;
   }) => ReactNode;
 }) {
+  const pathname = usePathname();
   const [headings, setHeadings] = useState<HTMLElement[]>([]);
   useEffect(() => {
     const nextHeadings = Array.from(document.querySelectorAll("h1, h2"));
     setHeadings(nextHeadings as HTMLElement[]);
-  }, []);
+  }, [pathname]);
   return headings.map((heading: HTMLElement) =>
     render({ heading, key: heading.id + heading.innerText }),
   );
