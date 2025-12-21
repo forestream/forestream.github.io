6c43975f168eb745129f2e521bcbc18860c5e201
hanbin <rj2303@naver.com>
2025-12-21 17:52:55 +0900

    fix style

--- app/_components/common/button.tsx
+++ app/_components/common/button.tsx
@@ -5,7 +5,7 @@ export function Button({ className, ...props }: ComponentProps<"button">) {
   return (
     <button
       className={twMerge(
-        "group relative cursor-pointer overflow-hidden rounded-[4px] p-[1px] before:absolute before:top-0 before:left-0 before:aspect-square before:w-[200%] before:-translate-x-1/4 before:-translate-y-1/4 before:border-2 before:border-transparent before:bg-[linear-gradient(to_right,var(--color-blue-700),red)] before:bg-origin-border before:transition-all before:duration-750 hover:before:rotate-360",
+        "group relative cursor-pointer overflow-hidden rounded-[4px] p-0.25 before:absolute before:top-0 before:left-0 before:aspect-square before:w-[200%] before:-translate-x-1/4 before:-translate-y-1/4 before:border-2 before:border-transparent before:bg-[linear-gradient(to_right,var(--color-blue-700),red)] before:bg-origin-border before:transition-all before:duration-750 hover:before:rotate-360",
         className,
       )}
       {...props}
@@ -17,7 +17,7 @@ function Sheet({ className, ...props }: ComponentProps<"div">) {
   return (
     <div
       className={twMerge(
-        "relative z-10 rounded-[3px] bg-white px-2 transition-opacity duration-500 group-hover:opacity-95",
+        "relative z-10 rounded-[3px] bg-white px-2.5 py-1.5 leading-none transition-opacity duration-500 group-hover:opacity-90",
         className,
       )}
       {...props}
